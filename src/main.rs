c629546f32720fdd120ad350b86d04c50c5d2098
ZHAO Zhenhua <zhao.zhenhua@qq.com>
2021-08-06 23:40:52 +0800

    v0.1

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,31 @@
 use json::{self, object};
 use reqwest;
+use serde::de::value::Error;
 use std::collections::HashMap;
+use std::fmt::{self,format};
 // use std::fs::{OpenOptions, File, write};
 use tokio::fs::File;
 use tokio::io::AsyncWriteExt;
 
 #[tokio::main]
 async fn main() -> Result<(), reqwest::Error> {
-    // let mut map = HashMap::new();
-    // map.insert("jsonrpc", "2.0");
-    // // map.insert("method", "getBlockByNumber");
-    // map.insert("method", "getBlockNumber");
-    // // map.insert("params", format!(b#"[1,"{}", true]"#, 0x1a_u64));
-    // map.insert("params", r#"[1]"#);
-    // // map.insert("params", r#"[1,"0x1a", true]"#);
-    // map.insert("id", "1");
-
-    let post_body = object!{
+    let post_body = object! {
         jsonrpc: String::from("2.0"),
         method: String::from("getBlockByNumber"),
         id: 1,
         params: [1, "0x1a", true]
     };
 
-    // println!("map in json = {} ", post_body.dump());
-
     let client = reqwest::Client::new();
-    // let resp = client
-    //     .post("http://127.0.0.1:8545")
-    //     .body(post_body.dump())
-    //     .send()
-    //     .await?;
-    
-    // println!("content = {:?}", resp.text().await);
 
-    match client.post("http://127.0.0.1:8545").body(post_body.dump()).send().await {
+    // // println!("content = {:?}", resp.text().await);
+
+    match client
+        .post("http://127.0.0.1:8545")
+        .body(post_body.dump())
+        .send()
+        .await
+    {
         Ok(resp) => match resp.text().await {
             Ok(text) => {
                 println!("RESPONSE: {} bytes received {:?} ", text.len(), text);
@@ -52,4 +43,59 @@ async fn main() -> Result<(), reqwest::Error> {
     }
 
     Ok(())
+}
+
+async fn fetch_block(blockId: u128) -> Result<(), reqwest::Error> {
+
+    let block = Block::new(blockId);
+
+    let post_body = object! {
+        jsonrpc: String::from("2.0"),
+        method: String::from("getBlockByNumber"),
+        id: 1,
+        params: [1, block.hex_height, true]
+    };
+
+    let client = reqwest::Client::new();
+
+    match client
+        .post("http://127.0.0.1:8545")
+        .body(post_body.dump())
+        .send()
+        .await
+    {
+        Ok(resp) => match resp.text().await {
+            Ok(text) => {
+                println!("RESPONSE: {} bytes received {:?} ", text.len(), text);
+                match File::create(block.file_name).await {
+                    Ok(mut file) => {
+                        file.write_all(text.as_bytes()).await;
+                        ()
+                    }
+                    Err(err) => panic!("Unable to create file {}: {}", "0x1a.json", err,),
+                }
+            }
+            Err(err) => panic!("Unable to create file {}: {}", "0x1a.json", err,),
+        },
+        Err(err) => panic!("Unable to create file {}: {}", "0x1a.json", err,),
+    }
+    Ok(())
+}
+
+pub struct Block {
+    pub hex_height: String,
+    pub file_name: String,
+}
+
+impl Block {
+    fn new(height:u128) -> Self {
+        Block {
+            hex_height: hex_block_height(height),
+            file_name: format!("{}.json", hex_block_height(height)),
+        }
+    }
+}
+
+fn hex_block_height( height:u128) -> String {
+    format!("{:#08x}", height)
 }
