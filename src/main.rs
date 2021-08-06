f22afa1805c58acca68760a1779cc6fd826e0576
ZHAO Zhenhua <zhao.zhenhua@qq.com>
2021-08-06 20:55:04 +0800

    async

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,10 @@
 use json;
 use reqwest;
 use std::collections::HashMap;
-use std::fmt;
-use std::io::Write;
 // use std::fs::{OpenOptions, File, write};
 use tokio::fs::File;
 use tokio::io::AsyncWriteExt;
-// #[macro_use]
-// extern crate serde;
-// extern crate serde_derive;
-// extern crate reqwest;
-// use std::collections::HashMap;
+
 
 #[tokio::main]
 async fn main() -> Result<(), reqwest::Error> {
@@ -23,9 +17,9 @@ async fn main() -> Result<(), reqwest::Error> {
 
     let client = reqwest::Client::new();
     match client.post("http://127.0.0.1:8545").json(&map).send().await {
-        Ok(resp) => match resp.text().await {
+        Ok(resp) => match resp.text_with_charset("utf-8").await {
             Ok(text) => {
-                // File::create("path")
+                println!("RESPONSE: {} bytes received ", text.len());
                 match File::create("0x1a.json").await {
                     Ok(mut file) => {
                         file.write_all(text.as_bytes());
