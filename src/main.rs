c7d8bbd35da8248a1768c57879c119b52043e3c3
ZHAO Zhenhua <zhao.zhenhua@qq.com>
2021-08-06 23:43:51 +0800

    v0.1

--- src/main.rs
+++ src/main.rs
@@ -9,40 +9,7 @@ use tokio::io::AsyncWriteExt;
 
 #[tokio::main]
 async fn main() -> Result<(), reqwest::Error> {
-    let post_body = object! {
-        jsonrpc: String::from("2.0"),
-        method: String::from("getBlockByNumber"),
-        id: 1,
-        params: [1, "0x1a", true]
-    };
-
-    let client = reqwest::Client::new();
-
-    // // println!("content = {:?}", resp.text().await);
-
-    match client
-        .post("http://127.0.0.1:8545")
-        .body(post_body.dump())
-        .send()
-        .await
-    {
-        Ok(resp) => match resp.text().await {
-            Ok(text) => {
-                println!("RESPONSE: {} bytes received {:?} ", text.len(), text);
-                match File::create("0x1a.json").await {
-                    Ok(mut file) => {
-                        file.write_all(text.as_bytes()).await;
-                        ()
-                    }
-                    Err(err) => panic!("Unable to create file {}: {}", "0x1a.json", err,),
-                }
-            }
-            Err(err) => panic!("Unable to create file {}: {}", "0x1a.json", err,),
-        },
-        Err(err) => panic!("Unable to create file {}: {}", "0x1a.json", err,),
-    }
-
-    Ok(())
+    fetch_block(01).await
 }
 
 async fn fetch_block(blockId: u128) -> Result<(), reqwest::Error> {
