ea34e028a6c889afdd1b0ddc46042aeb4162bb05
ZHAO Zhenhua <zhao.zhenhua@qq.com>
2021-08-06 21:05:20 +0800

    async

--- src/main.rs
+++ src/main.rs
@@ -12,15 +12,15 @@ async fn main() -> Result<(), reqwest::Error> {
     map.insert("jsonrpc", "2.0");
     map.insert("method", "getBlockByNumber");
     // map.insert("params", format!(b#"[1,"{}", true]"#, 0x1a_u64));
-    map.insert("params", r#"[1,"0x1a", true]"#);
+    map.insert("params", r#"[1,"0x02", true]"#);
     map.insert("id", "1");
 
     let client = reqwest::Client::new();
     match client.post("http://127.0.0.1:8545").json(&map).send().await {
-        Ok(resp) => match resp.text_with_charset("utf-8").await {
+        Ok(resp) => match resp.text().await {
             Ok(text) => {
                 println!("RESPONSE: {} bytes received ", text.len());
-                match File::create("0x1a.json").await {
+                match File::create("0x02.json").await {
                     Ok(mut file) => {
                         file.write_all(text.as_bytes());
                         ()
